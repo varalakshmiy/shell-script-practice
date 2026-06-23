bd9570efeae72bd64b6f935243893a32bb3502ef
varalakshmi <vv@gmail.com>
2026-06-23 14:30:55 +0530

    welcome mesg

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 
-person1=trump
-person2=trump2
-echo "$person1 :welcome"
-echo "$person1 :welcome1"
-echo "$person2 :welcome2"
-echo "$person2 :welcome3"
+PERSON1=trump
+PERSON2=trump2
+echo "$PERSON1 : welcome"
+echo "$PERSON1 : welcome1"
+echo "$PERSON2 : welcome2"
+echo "$PERSON2 : welcome3"
