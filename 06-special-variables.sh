0418d336723d9f2d5290d3ac09fa81d529724811
varalakshmi <vv@gmail.com>
2026-06-23 21:06:21 +0530

    special variables

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -3,3 +3,8 @@
 # special variables
 echo "all variables passed to script: $@"
 echo "n of variables passed: $#"
+echo "first number: $1"
+echo "script name: $0"
+echo "which user running this: $USER"
+
+
