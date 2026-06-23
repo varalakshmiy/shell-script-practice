ef7bc3aa23e8a803e8de3c8206e02cb1c5f67a19
varalakshmi <vv@gmail.com>
2026-06-23 16:23:16 +0530

    variables

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-echo "Time is : $TIMESTAMP"
+# TIMESTAMP=$(date)
+# echo "Time is : $TIMESTAMP"
+
+    START_TIME=$(date +%s)
+    sleep 10
+    END_TIME=$(date +%s)
+
+    total_time=$(($END_TIME-$START_TIME))
+    echo "total time $total_time"
