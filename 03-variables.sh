0b1f41a6198aff0024ba372c9604f5a2e48f34ce
varalakshmi <vv@gmail.com>
2026-06-27 10:10:33 +0530

    Fix submodule issue

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,5 +2,12 @@
 #!/bin/bash
  
  echo "pls enter username"
+ read USER_NAME 
+ echo "username is $USER_NAME"
+
+ echo "pls enter password"
+ read -s PASSWORD
+
+ echo "password is:$PASSWORD"
 
 
--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -2,9 +2,9 @@
 
 # special variables
 echo "all variables passed to script: $@"
-echo "n of variables passed: $#"
-echo "first number: $1"
-echo "script name: $0"
-echo "which user running this: $USER"
+# echo "n of variables passed: $#"
+# echo "first number: $1"
+# echo "script name: $0"
+# echo "which user running this: $USER"
 
 
